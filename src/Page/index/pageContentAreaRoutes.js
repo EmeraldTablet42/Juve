8357aa464238a3a979c7c43e33ce9731013210b2
gogeonyoung <rjsdud4574@gmail.com>
2023-08-22 11:44:13 +0900

    20230822

--- src/Page/index/pageContentAreaRoutes.js
+++ src/Page/index/pageContentAreaRoutes.js
@@ -7,6 +7,7 @@ import HomePage from "../contentPage/homePage/homePage";
 import Join from "../contentPage/member/join";
 import Login from "../contentPage/member/login";
 import { LoginUnsigned } from "../contentPage/member/loginUnsigned";
+import Detailmenu from "../contentPage/categorypage/detailmenu";
 const PageContentAreaRoutes = () => {
   return (
     <div>
@@ -18,6 +19,7 @@ const PageContentAreaRoutes = () => {
         <Route path="/member/join" element={<Join />} />
         <Route path="/member/login" element={<Login />} />
         <Route path="/loginUnsigned" element={<LoginUnsigned />} />
+        <Route path="detail" element={<Detailmenu />} />
       </Routes>
     </div>
   );
