ad5befab6e06255805aabc4303c2e7a5741bc25c
gogeonyoung <rjsdud4574@gmail.com>
2023-08-24 18:01:48 +0900

    202308240601

--- src/Page/contentPage/categorypage/components/thumbnail.js
+++ src/Page/contentPage/categorypage/components/thumbnail.js
@@ -5,28 +5,30 @@ import { Link } from 'react-router-dom';
 import Detailmenu from '../detailmenu';
 const Thumbnail = ({productData}) => {
   const [popupState, setPopupState] = useState(false);
-  
-  
+  const [selectedData, setSelectedData] = useState([]);
   const goToCart = () => {
     setPopupState(true);
   }
   if (!productData || productData.length === 0) {
     return null;
   }
+
+  const handleImageClick = (product) => {
+    setSelectedData(product);
+    alert(JSON.stringify(selectedData));
+  };
   return (
     <div className='product-thumbnail-wrapper'>
        <div className='product-thumbnail-grid'>
         {productData.map((product) => (
-          <div className='product-thumbnail' key={product.productPrice}>
+          <div className='product-thumbnail' key={product.productCode}>
             <Link to='/detail' style={{ display: 'inline-block' }}>
-              <div
-                className='product-thumbnail-image'
-                style={{
-                  backgroundImage: `http://localhost:8090/product/photo/${product.productPhoto} ?? "상품이미지"})`,
-                  width: '200px',
-                  height: '200px',
-                }}
-              ></div>
+                <img
+                 src={`http://localhost:8090/product/photo/${product.productPhoto}`}
+                 alt='상품 이미지' 
+                 style={{ width: '200px',height: '200px',}}
+                 onClick={() => handleImageClick(product)}
+                />
             </Link>
             <h3 style={{textAlign:"center"}}>{product.productName}</h3>
             <div className='product-thumbnail-detail'>
@@ -36,11 +38,12 @@ const Thumbnail = ({productData}) => {
             </div>
             {popupState && (
               <div className='cart-popup-wrapper'>
-                <Popupcart productId={product.productCode} setPopupState={setPopupState} userInfo = {{username: '고건영', userPk: 1}} />
+                <Popupcart productData={productData} productId={product.productCode} setPopupState={setPopupState} userInfo = {{username: '고건영', userPk: 1}} />
               </div>
           )}
         </div>
       ))}
+        {false && selectedData && <Detailmenu productData={[selectedData]}/> }
       </div>
     </div>
   )
--- src/Page/contentPage/categorypage/detailmenu.js
+++ src/Page/contentPage/categorypage/detailmenu.js
@@ -1,19 +1,40 @@
-import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 
-const Detailmenu = (props) => {
-  const { productData } = props;
-
+const Detailmenu = (productData) => {
+  const DataArray = Object.values(productData);
+  const [selectedOptions, setSelectedOptions] = useState([]);
+  
+  const handleOptionChange = (productName, productPrice, category) => {
+    const updatedOptions = selectedOptions.filter(
+      (option) => option.product.productName !== productName);
+      if (productPrice >0) {
+        const updatedOption = {category, productName, productPrice};
+        setSelectedOptions([...updatedOptions, updatedOption])
+      }
+      else{
+        setSelectedOptions(updatedOptions);
+      }
+  }
   return (
     <div className='Detail-wrapper'>
-      {productData.map((product) => (
-        <div className='Detail-item' key={product.productid}>
+      {DataArray.map((item) => (
+        <div className='Detail-item' key={item.productNum}>
           <div className='Detail-image'>
-            <img src={product.productPhoto} alt='상품 이미지' />
+            <img src={item.productPhoto} alt='상품 이미지' />
           </div>
           <div className='Detail-option'>
-            <p>상품명 : {product.productName}</p>
-            <p>가 격 : {product.productPrice}</p>
+            <p>상품명 : {item.productName}</p>
+            <p>가 격 : {item.productPrice}</p>
+            <select key={item.category}
+            onChange={(e)=>
+            handleOptionChange(item.productName, e.target.value, e.target.selectedOptions[0].getAttribute('data-price'))}>
+              <option value="" hidden>
+                {item.productName} 선택
+              </option>
+              <option key={item.Num} value={item.productName} data-price={item.productPrice}>
+                {item.productName}
+              </option>
+            </select>
           </div>
         </div>
       ))}
