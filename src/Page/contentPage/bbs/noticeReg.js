554c92471c27b1427d2d687e465e79639156016b
ET <emeraldtablet42@gamil.com>
2023-08-31 16:57:48 +0900

    게시판 ckedtor5

--- src/Page/contentPage/bbs/noticeReg.js
+++ src/Page/contentPage/bbs/noticeReg.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import "./ckeditor5.css";
 
 const NoticeReg = () => {
+  const [editorData, setEditorData] = useState("");
   return (
     <>
       <h2>공지사항 등록</h2>
+      <CKEditor
+        editor={ClassicEditor}
+        data={editorData}
+        onChange={(event, editor) => {
+          const data = editor.getData();
+          setEditorData(data);
+        }}
+      />
+      <button
+        onClick={() => {
+          alert(editorData);
+        }}
+      >
+        내용확인
+      </button>
     </>
   );
 };
