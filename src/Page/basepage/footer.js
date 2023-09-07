91b663f56d0f51673b4ec7d5f699bd4e71d8e3a9
gogeonyoung <rjsdud4574@gmail.com>
2023-09-08 03:36:36 +0900

    footer sample

--- src/Page/basepage/footer.js
+++ src/Page/basepage/footer.js
@@ -1,11 +1,15 @@
-import React from 'react'
-
+import React from 'react';
+import juveimage from '../imagefile/juvefooter.png';
+import './footer.css';
 const Footer = () => {
   return (
-   <>
-   회사소개
-   </>
-  )
-}
+    <div className="footer-wrapper">
+      <div className="company-logo">
+        <img src={juveimage} alt="로고" />
+      </div>
+      <div className="company-info">회사정보</div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
