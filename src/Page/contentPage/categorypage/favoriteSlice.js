7e54b67b1308e80da7f98b62c84426b15cf4420f
ET <emeraldtablet42@gamil.com>
2023-09-08 02:10:35 +0900

    찜상품

---
+++ src/Page/contentPage/categorypage/favoriteSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+import sampleimage from "../../../imagefile/carticon.png";
+const initialState = [];
+
+const favoriteSlice = createSlice({
+  name: "fav",
+  initialState,
+  reducers: {
+    setFavorite: (state, action) => {
+      return action.payload;
+    },
+    removeFavorite: (state, action) => {
+      // action.payload에 제거하려는 값을 전달받아서 배열에서 제거합니다.
+      return state.filter((item) => item !== action.payload);
+    },
+    addFavorite: (state, action) => {
+      // action.payload에 추가하려는 값을 전달받아서 배열에 추가합니다.
+      state.push(action.payload);
+    },
+  },
+});
+
+export const { setFavorite, removeFavorite, addFavorite } =
+  favoriteSlice.actions;
+
+export default favoriteSlice.reducer;
